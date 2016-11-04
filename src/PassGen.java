ad1bdc5975ccd921b55fd8ef0325542405957409
OlegKKK <kramo@ukr.net>
2016-11-04 14:25:56 +0200

    Added random generator

--- src/PassGen.java
+++ src/PassGen.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class PassGen extends JFrame {
+public class PassGen extends JFrame implements ActionListener, ItemListener{
 
     public JTextField textOutput;
     public JTextField lengthOfPass;
@@ -10,31 +14,39 @@ public class PassGen extends JFrame {
     public JButton shuffleOutput;
     public JButton copyOutputToClipboard;
 
-    public JCheckBox includeCaps;
-    public JCheckBox includeLowerCase;
-    public JCheckBox includeNumbers;
-    public JCheckBox includeSpecialCharacters;
+    public JCheckBox includeCapsCB;
+    public JCheckBox includeLowerCaseCB;
+    public JCheckBox includeNumbersCB;
+
+    public boolean includeCaps;
+    public boolean includeLowerCase;
+    public boolean includeNumbers;
+
 
     public PassGen() {
         super("Random Password Generator");
 
         textOutput = new JTextField(30);
+        textOutput.addActionListener(this);
         textOutput.setEnabled(false);
         lengthOfPass = new JTextField("8",2);
+        lengthOfPass.addActionListener(this);
 
         generatePass = new JButton("Generate Password");
+        generatePass.addActionListener(this);
+
         shuffleOutput = new JButton("Shuffle Output");
         copyOutputToClipboard = new JButton("Copy To Clipboard");
 
         JPanel optionsPanel = new JPanel(new FlowLayout());
 
-        includeCaps = new JCheckBox("Include Caps", true);
-        includeLowerCase = new JCheckBox("Include Lower Case Characters", true);
-        includeNumbers = new JCheckBox("Include Numbers", true);
+        includeCapsCB = new JCheckBox("Include Caps", true);
+        includeLowerCaseCB = new JCheckBox("Include Lower Case Characters", true);
+        includeNumbersCB = new JCheckBox("Include Numbers", true);
 
-        optionsPanel.add(includeCaps);
-        optionsPanel.add(includeLowerCase);
-        optionsPanel.add(includeNumbers);
+        optionsPanel.add(includeCapsCB);
+        optionsPanel.add(includeLowerCaseCB);
+        optionsPanel.add(includeNumbersCB);
         optionsPanel.add(new JLabel("Length in Characters: "));
         optionsPanel.add(lengthOfPass);
 
@@ -53,6 +65,33 @@ public class PassGen extends JFrame {
     }
 
     public static void main(String[] args) {
+        new PassGen();
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        int length;
+
+        try{
+            length = Integer.parseInt(lengthOfPass.getText());
+        } catch (NumberFormatException nuEx) {
+            length = 8;
+        }
+        textOutput.setText(RandomGenerator.nextWord(length,includeCaps,includeLowerCase,includeNumbers));
+    }
+    public void itemStateChanged(ItemEvent e) {
+        Object source = e.getItemSelectable();
+
+        if (source.equals(includeCapsCB)) {
+            includeCaps = !includeCaps;
+            System.out.println("" + includeCaps);
+        } else if (source.equals(includeLowerCaseCB)) {
+            includeLowerCase = !includeLowerCase;
+            System.out.println("" + includeLowerCase);
+        } else if (source.equals(includeNumbersCB)) {
+            includeNumbers = !includeNumbers;
+            System.out.println("" + includeNumbers);
+        }
     }
 }
--- src/RandomGenerator.java
+++ src/RandomGenerator.java
@@ -5,6 +5,7 @@ public class RandomGenerator {
     public static Random intGenerator = new Random();
 
     public static String nextWord(int length, boolean includeCaps, boolean includelowerCase, boolean includeNumbers) {
+        String word = "";
 
         ArrayList<String> charTypes = new ArrayList();
         if (includeCaps) {
@@ -21,15 +22,22 @@ public class RandomGenerator {
             String charType = charTypes.get(intGenerator.nextInt(charTypes.size()));
             System.out.println(charType);
             char charType2 = charType.charAt(0);
+
+            //x = minRange
+            //y = maxRange
+            //nextInt(y-x) + x;
             switch (charType2){
                 case 'c':
+                word+=(char) intGenerator.nextInt(90-65) + 65;
                     break;
                 case 'l':
+                word+=(char) intGenerator.nextInt(122-97) + 97;
                     break;
                 case 'n':
+                word+=(char) intGenerator.nextInt(48-57) + 57;
                     break;
             }
         }
-        return "null";
+        return word;
     }
 }
